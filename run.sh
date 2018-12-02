d11883fd1ac2d323b5dc63ca286aaaccf52f33a1
breezedeus <breezedeus@163.com>
2018-12-02 18:32:16 +0800

    微调

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 export BERT_BASE_DIR='/Users/king/Documents/Ein/语料/BERT/chinese_L-12_H-768_A-12'
 TEST_DATA_DIR='./data'
 DPF_DIR='/Users/king/Documents/Ein/Codes/dpf/data/projects/botlet_insurance/cv_0'
-export BERT_SAMPLE_NUM=100
+export BERT_TRAIN_SAMPLE_NUM=100
+export BERT_DEV_SAMPLE_NUM=100  # dev 和 test 数据集都用这个值
 
 ## create pretraining data
 #python create_pretraining_data.py \
@@ -28,10 +29,10 @@ export BERT_SAMPLE_NUM=100
 #  --vocab_file=$BERT_BASE_DIR/vocab.txt \
 #  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
 #  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-#  --max_seq_length=128 \
-#  --train_batch_size=32 \
+#  --max_seq_length=50 \
+#  --train_batch_size=16 \
 #  --learning_rate=5e-5 \
-#  --num_train_epochs=20.0 \
+#  --num_train_epochs=200.0 \
 #  --output_dir=$TEST_DATA_DIR/bert_classifier/
 
 # run classifier on qabot data
@@ -44,8 +45,8 @@ python run_classifier.py \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-  --max_seq_length=128 \
+  --max_seq_length=100 \
   --train_batch_size=32 \
   --learning_rate=5e-5 \
-  --num_train_epochs=2.0 \
+  --num_train_epochs=20.0 \
   --output_dir=$TEST_DATA_DIR/qabot_classifier/
